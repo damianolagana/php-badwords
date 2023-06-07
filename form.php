bc686a458876444c600b6e154547713399e151a9
damianolagana <damianolagana@gmail.com>
2023-06-07 16:55:03 +0200

    print paragraph from FORM

--- form.php
+++ form.php
@@ -11,8 +11,8 @@
     <main>
         <div class="formBox">
             <form action="printData.php" method="GET">
-                <input type="text" name="nome">
-                <input type="text" name="parolaCensurata">
+                <input class="h50px" type="text" name="paragrafo" size="40" maxlength="200" placeholder="Inserisci del testo">
+                <input type="text" name="parolaCensurata" size="30" placeholder="Inserisci parola da censurare">
                 <button type="submit">Invia</button>
             </form>
 
